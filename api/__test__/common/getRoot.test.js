f8f127886df6faf063dc49a24d41199077ddd398
estellar_boyz <tayalone@gmail.com>
2020-05-09 17:39:01 +0700

    :white_check_mark: [ADD] test GET :/

---
+++ api/__test__/common/getRoot.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('../../server')
+describe('Common GET :/', () => {
+  let res = null
+  beforeAll(async () => {
+    res = await request(app).get('/')
+  })
+  test('statusCode should be 200', async () => {
+    expect(res.statusCode).toEqual(200)
+  })
+  test('data.message should be OK', async () => {
+    expect(res.body.message).toEqual('OK')
+  })
+})
