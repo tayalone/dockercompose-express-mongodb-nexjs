f61fca3e2fb63b402a02dffbba8292cd3b437d45
estellar_boyz <tayalone@gmail.com>
2020-05-09 17:40:35 +0700

    :building_construction: [UPDATE] updated __test__ folder

--- api/__test__/integrations/common/getRoot.test.js
+++ api/__test__/integrations/common/getRoot.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../server')
+const app = require('../../../server')
 describe('Common GET :/', () => {
   let res = null
   beforeAll(async () => {
