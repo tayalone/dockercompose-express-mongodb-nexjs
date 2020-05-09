b43a27fd8d5acce3997bb2e601069a57bb6759f4
estellar_boyz <tayalone@gmail.com>
2020-05-09 14:32:15 +0700

    :white_check_mark: [ADD] added test utils/common

--- api/utils/common/divide.js
+++ api/utils/common/divide.js
@@ -1,17 +1,10 @@
 const commontErrMessage = 'error from /utils/common/divide'
 const commontErrKey = 'error_from_/utils/common/divide'.toUpperCase()
 
-const divide = async (dividend, divisor) => {
-  try {
-    const result = dividend / divisor
-    const portion = dividend % divisor
-    return { result, portion }
-  } catch (e) {
-    return res.status(400).send({
-      errMessage: commontErrMessage,
-      errKey: commontErrKey
-    })
-  }
+const divide = (dividend, divisor) => {
+  const result = Math.floor(dividend / divisor)
+  const portion = dividend % divisor
+  return { result, portion }
 }
 
 module.exports = { divide, commontErrMessage, commontErrKey }
